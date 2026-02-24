8a75d7b999b21a3a90c3bef3cb935086da922aa4
TrangPham93 <hatrangc2@gmail.com>
2026-02-24 12:18:29 +0200

    fix the font and add CV

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,5 @@
+import cvFile from "../assets/CV_Trang_Pham_2025.pdf"
+
 export default function About() {
 	return (
 		<div className="page">
@@ -12,25 +14,39 @@ export default function About() {
 
 					I'm experienced in financial data analysis, reports building and automating workflows to reduce errors and increase efficiency.<br/>
 
-					My other hobbies are swimming 🧜‍♀️, yoga 🧘‍♀️ and crocheting🧶.
 				</p	>
+			</section>
+
+			<section className="CV">
+				<h3>Download my CV</h3>
+				<ul>
+					<a
+						href={cvFile}
+						download="TrangPham_CV.pdf"
+						className="cv-button"
+					>
+						Download (PDF)
+					</a>
+				</ul>
 			</section>
 			
 			<section className="section">
 				<h1>Experience</h1>
-				<p>
-					Studying full-time at Hive Coding School, building projects in C, 
-					Makefile, Git, React, Typescript, and UNIX environment.
-				</p>
+				<ul>
+					<li> Built individual and team projects in C, C++, Makefile. </li>
+					<li> Worked frequently with Git and UNIX environment. </li>
+					<li> Experienced with Docker, virtual machine </li>
+					<li> Self-learned Python, Tableau, PowerBi. </li>
+				</ul>
 			</section>
 
 			<section className="section">
-				<h1>Skills</h1>
+				<h1>Soft skills</h1>
 				<ul className="tag-list">
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
+					<li> Teamwork skill in school projects and in Finnish company</li>
+					<li> Time management </li>
+					<li> Adapt learning </li>
+					<li> Clear communication </li>
 				</ul>
 			</section>
 
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -2,6 +2,7 @@ export default function Blog() {
 	return (
 		<div>
 			<h1>My blog</h1>
+			<p> To be updated </p>
 		</div>
 	)
 }
